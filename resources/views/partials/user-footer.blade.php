9b85c325953cacde8751cdc003090157d22c1964
Muhammad Hidayat <hd4y2t@gmail.com>
2023-05-25 16:56:22 +0700

    push

---
+++ resources/views/partials/user-footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="bg-dark py-3">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p class="text-muted text-center mb-0">&copy; 2021 Copyright<i class="mdi mdi-heart text-danger"></i> All
+                    Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
